bb517901d95e4a7c6976346f66694cb7694c4d4b
bartus <szczepaniak.bartek+github@gmail.com>
2022-03-14 11:19:08 +0100

    Update posting scheme

--- aur-post-comment-with-github-message.sh
+++ aur-post-comment-with-github-message.sh
@@ -18,7 +18,8 @@ _message_eval_template='
 #  define evals for `curl`
 _curl='curl -L -s -b /tmp/aur_cookie.txt "https://aur.archlinux.org/pkgbase/${pkgname}/"'
 # post comment needs: token, comment, ID ( can be anything, AUR isn't chacking it )
-_curl_post_comment=$_curl' -d "action=do_AddComment&ID=${ID}&token=${token}" --data-urlencode "comment=${message}"'
+#_curl_post_comment=$_curl' -d "action=do_AddComment&ID=${ID}&token=${token}" --data-urlencode "comment=${message}"'
+_curl_post_comment='curl -b /tmp/aur_cookie.txt "https://aur.archlinux.org/pkgbase/${pkgname}/comments" --data-urlencode "comment=${message}"'
 # pin comment needs: token, comment_id
 _curl_pin_comment=$_curl' -d "action=do_PinComment&comment_id=${comment_id}&token=${token}"'
 
@@ -31,12 +32,10 @@ pkgname=$(. PKGBUILD; echo $pkgname)
 # get cookie with: curl -c cookie.txt -d "user=" -d "password=" https://aur.archlinux.org/login
 [ -f /tmp/aur_cookie.txt ] || {
   read -p "password for ${aur_username}@aur:" pass
-  curl -s -c /tmp/aur_cookie.txt 'https://aur.archlinux.org/login' -d "user=${aur_username}&passwd=$pass" >/dev/null
+  curl -c /tmp/aur_cookie.txt 'https://aur.archlinux.org/login' -H 'referer: https://aur.archlinux.org/login' -d "user=${aur_username}&passwd=$pass&next=/" >/dev/null
 }
 
 # get post token/ID by probing comment FORM
-token=$(eval ${_curl}|grep -Po 'name="token" value="\K[a-z0-9]*'|head -n1)
-ID=$(eval ${_curl}|grep -Po 'name="ID" value="\K[a-z0-9]*'|head -n1)
 # post a comment
 eval ${_curl_post_comment}
 # get $comment_id (latest comment_id will be last in the numerical order)
